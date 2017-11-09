0e17e44084e842c049a34f5e4498bcf68d8778cd
Avigail F <mazehle.ah@gmail.com>
2017-11-09 01:53:54 -0500

    There adding location

--- app/src/main/java/com/minyan/avigailfohrman/candelighting/MainActivity.java
+++ app/src/main/java/com/minyan/avigailfohrman/candelighting/MainActivity.java
@@ -1,17 +1,53 @@
 package com.minyan.avigailfohrman.candelighting;
 
-import android.icu.util.HebrewCalendar;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    StringBuilder sunsetApiUrl;
+    String urlBase = "https://api.sunrise-sunset.org/json?";
+    String latitudeName = "lat";
+    String longitudeName = "lng";
+    String defaultDate = "date=today";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sunsetApiUrl = new StringBuilder(urlBase);
+    }
 
+    private Date calculateCandlelighting() {
+        Date date = new Date();
+        return date;
+    }
+
+    private Date getSunset() {
+        buildSunsetUrlString();
+        // Call to the
+        Date date = new Date();
+
+
+        //date.setTime();
+        return date;
+    }
+
+    private void buildSunsetUrlString() {
+        double latitudeValue = getLocation().getLatitude();
+        double longitudeValue = getLocation().getLongitude();
+
+        sunsetApiUrl.append(latitudeName).append("=").append(latitudeValue);
+        sunsetApiUrl.append("&");
+        sunsetApiUrl.append(longitudeName).append("=").append(longitudeValue);
+        sunsetApiUrl.append(defaultDate);
+    }
+
+    public Location getLocation() {
+        Location location = getLocation();
+
+        return location;
     }
 }
