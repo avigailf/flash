174b59ee3b09614921db2361ffef51b60477bfe0
Avigail F <mazehle.ah@gmail.com>
2017-11-12 16:28:04 -0500

    Implemented Json request and volley and tried HttpsRequest x work

--- app/src/main/java/com/minyan/avigailfohrman/candelighting/MainActivity.java
+++ app/src/main/java/com/minyan/avigailfohrman/candelighting/MainActivity.java
@@ -1,23 +1,71 @@
 package com.minyan.avigailfohrman.candelighting;
 
 import android.location.Location;
+import android.location.LocationProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
-    StringBuilder sunsetApiUrl;
+    StringBuilder sunsetApiUrlStringBuilder;
     String urlBase = "https://api.sunrise-sunset.org/json?";
+    RequestQueue requestQueue;
+    URL sunsetUrl;
     String latitudeName = "lat";
     String longitudeName = "lng";
     String defaultDate = "date=today";
+    StringBuilder urlResponse = new StringBuilder();
+    TextView candleLightingTimeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sunsetApiUrl = new StringBuilder(urlBase);
+        sunsetApiUrlStringBuilder = new StringBuilder(urlBase);
+        candleLightingTimeTextView = findViewById(R.id.candlelighting_time);
+        candleLightingTimeTextView.setText("6:45");
+        buildSunsetUrlString();
+
+        requestJsonSunsetData();
+
+        //populateCandleLightingTimeTextView(candleLightingTimeTextView);
+    }
+
+    private void requestJsonSunsetData() {
+        setUpRequestQueue();
+        handleJsonRequest();
+    }
+
+    private void setUpRequestQueue() {
+        requestQueue = Volley.newRequestQueue(this);
+    }
+
+    private void populateCandleLightingTimeTextView(TextView candleLightingTimeTextView) {
+        StringBuilder candleLightingTime = new StringBuilder();
+        candleLightingTime.append("6:34");
+        candleLightingTimeTextView.setText(candleLightingTime.toString());
     }
 
     private Date calculateCandlelighting() {
@@ -27,26 +75,71 @@ public class MainActivity extends AppCompatActivity {
 
     private Date getSunset() {
         buildSunsetUrlString();
-        // Call to the
+        // Call to the API.
         Date date = new Date();
 
+        return calculateCandelightingFromSunset(date);
+    }
 
-        //date.setTime();
-        return date;
+    private Date calculateCandelightingFromSunset(Date sunsetTime) {
+        sunsetTime.setTime(sunsetTime.getTime() - 18);
+        return sunsetTime;
     }
 
     private void buildSunsetUrlString() {
         double latitudeValue = getLocation().getLatitude();
         double longitudeValue = getLocation().getLongitude();
 
-        sunsetApiUrl.append(latitudeName).append("=").append(latitudeValue);
-        sunsetApiUrl.append("&");
-        sunsetApiUrl.append(longitudeName).append("=").append(longitudeValue);
-        sunsetApiUrl.append(defaultDate);
+        sunsetApiUrlStringBuilder.append(latitudeName).append("=").append(latitudeValue);
+        sunsetApiUrlStringBuilder.append("&");
+        sunsetApiUrlStringBuilder.append(longitudeName).append("=").append(longitudeValue);
+        sunsetApiUrlStringBuilder.append("&");
+        sunsetApiUrlStringBuilder.append(defaultDate);
+    }
+
+    private void createHttpsUrlConnection() {
+        try {
+            sunsetUrl = new URL(sunsetApiUrlStringBuilder.toString());
+            HttpsURLConnection urlConnection = (HttpsURLConnection) sunsetUrl.openConnection();
+            InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+            readStream(inputStream);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleJsonRequest() {
+        String sampleUrl = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today";
+        sunsetApiUrlStringBuilder = new StringBuilder(sampleUrl);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, sunsetApiUrlStringBuilder.toString(), null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String time = "6:47";
+                try {
+                    time = response.getString("sunset");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                candleLightingTimeTextView.setText(time);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 
     public Location getLocation() {
-        Location location = getLocation();
+        Location location = new Location("");
+
+        location.getLatitude();
 
         return location;
     }
